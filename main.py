f01c17a42d4bc1b0d02864fa0c391cf71e2e393f
ebrunet <eric,brunet28@gmail.com>
2020-11-24 22:30:33 -0500

    changed dummy predictions for LinearRegression with selected features

--- main.py
+++ main.py
@@ -1,13 +1,47 @@
-if __name__ == "__main__":
+import datetime as dt
 
-    import datetime as dt
+import pandas as pd
+from sklearn.linear_model import LinearRegression
 
-    import pandas as pd
+from loader import Loader
 
-    from loader import Loader
+loader = Loader()
 
-    loader = Loader()
+features = [
+    # "User Name",
+    # "Personal URL",
+    # "Profile Cover Image Status",
+    # "Profile Verification Status",
+    # "Profile Text Color",
+    # "Profile Page Color",
+    # "Profile Theme Color",
+    # "Is Profile View Size Customized?",
+    # "UTC Offset",
+    # "Location",
+    # "Location Public Visibility",
+    # "User Language",
+    # "Profile Creation Timestamp",
+    # "User Time Zone",
+    "Num of Followers",
+    "Num of People Following",
+    "Num of Status Updates",
+    "Num of Direct Messages",
+    # "Profile Category",
+    # "Avg Daily Profile Visit Duration in seconds",
+    # "Avg Daily Profile Clicks",
+    # "Profile Image",
+]
 
-    predictions = pd.read_csv("data/sample_submission.csv")
+X = loader.train[features]
+y = loader.train["Num of Profile Likes"]
 
-    predictions.to_csv("submissions/submission_{}".format(dt.datetime.now()))
+regressor = LinearRegression()
+regressor.fit(X=X, y=y)
+
+X_test = loader.test[features]
+predictions = regressor.predict(X_test)
+
+df = pd.DataFrame(
+    {"Id": X_test.index, "NumProfileLikes": predictions.round()}, dtype=int
+)
+df.to_csv("submissions/submission_{}".format(dt.datetime.now()), index=False)
