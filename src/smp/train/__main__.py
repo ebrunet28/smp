b31a7222bea7859eff05724e364cc696d5c16099
Marc-Antoine Belanger <mbelanger@explorance.com>
2020-11-28 16:24:00 -0500

    fix metric

--- src/smp/train/__main__.py
+++ src/smp/train/__main__.py
@@ -48,11 +48,6 @@ class ToDense(Base):
 
 
 def grid_search(model, parameters):
-    class PredictMinZero(model):
-        _model = model
-
-        def predict(self, *args, **kwargs):
-            return np.maximum(self._model.predict(self, *args, **kwargs), 0)
 
     loader = Loader()
 
@@ -86,10 +81,10 @@ def grid_search(model, parameters):
             (
                 "Grid Search",
                 GridSearchCV(
-                    PredictMinZero(),
+                    model(),
                     param_grid=parameters,
                     n_jobs=-1,
-                    scoring="neg_mean_squared_log_error",
+                    scoring="neg_mean_squared_error",
                 ),
             ),
         ],
@@ -101,9 +96,9 @@ def grid_search(model, parameters):
     pipe.fit(X_train, y_train)
     scores = pipe.steps[-1][-1].cv_results_
     pp = pprint.PrettyPrinter(depth=6)
-    pp.pprint(f"Mean scores: {scores['mean_test_score']}")
+    pp.pprint(f"Mean scores: {(-scores['mean_test_score'])**(1/2)}")
     pp.pprint(f"Best params{pipe.steps[-1][-1].best_params_}")
-    pp.pprint(f"Best score: {max(scores['mean_test_score'])}")
+    pp.pprint(f"Best score: {min((-scores['mean_test_score'])**(1/2))}")
 
     predictions = pipe.predict(loader.test)
 
@@ -120,6 +115,12 @@ if __name__ == "__main__":
         "fit_intercept": (True, False),
         "normalize": (True, False),
     }
+    # parameters ={
+    #     "n_neighbors": [4, 5, 6, 8, 10, 15, 20, 30, 40, 50, 75, 100, 150, 200, 250, 300, 400],
+    #     "weights": ("uniform", "distance"),
+    #     "leaf_size": range(1, 10, 2),
+    #     "p": range(1, 3),
+    # }
 
     _ = grid_search(LinearRegression, parameters)
     _.to_csv(
