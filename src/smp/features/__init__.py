327ce8de71d88b839bd66a1d479c0f75de312101
Marc-Antoine Belanger <mbelanger@explorance.com>
2020-11-28 19:27:16 -0500

    yaml

---
+++ src/smp/features/__init__.py
@@ -0,0 +1,6 @@
+from .categorical import *
+from .discrete import *
+from .elapsed_time import *
+from .float import *
+from .image import *
+from .rgb import *
--- tests/test_train.py
+++ tests/test_train.py
@@ -1,10 +1,6 @@
-from smp.train.__main__ import grid_search
-from sklearn.linear_model import LinearRegression
+from smp.train.__main__ import main
 
 
-def test_grid_search(download_data):
-    parameters = {
-        "fit_intercept": (True, False),
-        "normalize": (True, False),
-    }
-    grid_search(LinearRegression, parameters)
+def test_main(download_data):
+
+    main("example.yml")
